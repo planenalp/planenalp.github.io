21324e8ace776778eff28059625beb3b2e1c7071
planenalp <null>
2025-03-05 20:14:25 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [1](https://planenalp.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 2022 
-### :alarm_clock: 2025-03-06 04:07:56 
+### :alarm_clock: 2025-03-06 04:14:25 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/KleinBlueTheme.js
+++ docs/assets/KleinBlueTheme.js
@@ -104,6 +104,9 @@ document.addEventListener('DOMContentLoaded', function() {
             body {
                 padding: 8px !important;
             }
+            .postTitle{
+                font-size:24px !important;
+            }
         }
 
         `;
@@ -142,6 +145,12 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             .tagTitle {
                 display: none !important;
+            }
+            .LabelTime{
+                display:unset !important;
+            }
+            .LabelName{
+                display:none !important;
             }
         }
         
