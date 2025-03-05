ed2a74d0cddd3f879a85c211a369784ed7dd0f7c
planenalp <null>
2025-03-05 19:24:02 +0000

    🎉auto update by Gmeek action

--- docs/assets/KleinBlueTheme.js
+++ docs/assets/KleinBlueTheme.js
@@ -77,9 +77,6 @@ document.addEventListener('DOMContentLoaded', function() {
             #header h1 {
                 margin-top: 10px;
             }
-            #header h1 a {
-                font-size: 30px;
-            }
             .title-right {
                 margin-top: 50px;
             }
