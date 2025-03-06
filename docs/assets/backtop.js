03661d810bf4dd2bbc4ace24ecb9b4e39e4d9016
planenalp <null>
2025-03-06 13:04:14 +0000

    🎉auto update by Gmeek action

--- docs/assets/backtop.js
+++ docs/assets/backtop.js
@@ -4,37 +4,42 @@
   style.textContent = `
     .back-to-top {
       position: fixed;
-      bottom: 80px;
-      right: 15px;
-      background-color: rgba(255, 255, 255, 0.8);
-      color: #333;
-      border: none;
-      border-radius: 50%;
+      bottom: 120px;
+      right: 20px;
       cursor: pointer;
-      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-      width: 50px;
-      height: 50px;
-      font-size: 30px;
-      z-index: 10000;
-      transition: transform 0.2s ease, opacity 0.3s ease;
+      font-size: 24px;
+      background-color: #FFFFFFB3;
+      color: #656d76b3;
+      border: 2px solid #656d76b3;
+      border-radius: 50%;
+      width: 40px;
+      height: 40px;
       display: none;
       align-items: center;
       justify-content: center;
+      box-shadow: 0 1px 3px rgba(0,0,0,0.12);
+      z-index: 1000;
+      transition: transform 0.2s ease, opacity 0.3s ease;
+      user-select: none;
+      -webkit-tap-highlight-color: transparent;
+      outline: none;
     }
     .back-to-top:hover {
       transform: scale(1.1);
+      color: #FFFFFFB3;
+      background-color: #81D8D0B3;
+      border-color: #FFFFFFB3; /* 激活按钮边框颜色改为白色 */
     }
     .back-to-top svg {
       width: 24px;
       height: 24px;
+      fill: none; /* 设置 svg 内部不填充颜色（透明） */
+      stroke: currentColor; /* 想要即时切换只能用 currentColor 将描边颜色设置为当前文字颜色（继承父元素的颜色）*/
+      stroke-width: 2; /* 设置描边（线条）的宽度为 2 像素 */
+      stroke-linecap: round; /* 设置描边端点为圆形，使线条末端圆润 */
+      stroke-linejoin: round;  /* 设置线条转角为圆形，使角部更平滑 */
     }
-    @media (max-width: 768px) {
-      .back-to-top {
-        width: 40px;
-        height: 40px;
-        font-size: 20px;
-      }
-    }
+
   `;
   document.head.appendChild(style);
 
