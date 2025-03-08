bcebd80f35594ef4122b52ed2a0816d0f5c8298c
planenalp <null>
2025-03-08 02:18:29 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [1](https://planenalp.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 2022 
-### :alarm_clock: 2025-03-08 10:14:17 
+### :alarm_clock: 2025-03-08 10:18:29 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/backtop.js
+++ docs/assets/backtop.js
@@ -27,10 +27,18 @@
     }
     .back-to-top:hover {
       transform: scale(1.1);
+      color: #8b949eb3;
+      background-color: #002fa7b3;
+      border-color: #8b949eb3;
     }
     .back-to-top svg {
       width: 24px;
       height: 24px;
+      fill: none; /* 设置 svg 内部不填充颜色（透明） */
+      stroke: currentColor; /* 想要即时切换只能用 currentColor 将描边颜色设置为当前文字颜色（继承父元素的颜色）*/
+      stroke-width: 2; /* 设置描边（线条）的宽度为 2 像素 */
+      stroke-linecap: round; /* 设置描边端点为圆形，使线条末端圆润 */
+      stroke-linejoin: round;  /* 设置线条转角为圆形，使角部更平滑 */
     }
 
   `;
