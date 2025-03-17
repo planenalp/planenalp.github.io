d3695db4347e63c6f47b2abeb5f0fc16cd528549
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-18 01:13:34 +0800

    Update ArticleJs.js

--- static/assets/ArticleJs.js
+++ static/assets/ArticleJs.js
@@ -1,12 +1,35 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const t = new IntersectionObserver(o => {
+        o.forEach(o => {
+            if (o.isIntersecting) {
+                const e = o.target,
+                      a = e.previousElementSibling;
 
-	//////////////// 引入fancybox所需的css文件以及所需的绑定函数 start ////////////////
-	document.head.appendChild(Object.assign(document.createElement('link'), {
-		rel: 'stylesheet',
-		href: 'https://planenalp.github.io/assets/fancybox.css'
-	}));
-	Fancybox.bind('[data-fancybox="gallery"]', {});
-	//////////////// 引入fancybox所需的css文件以及所需的绑定函数 end ////////////////
+                const i = (t = !1) => {
+                    if (a && a.classList.contains("ImgLazyLoad-circle")) {
+                        a.style.display = "none";
+                    }
+                    
+                    if (t) {
+                        const t = document.createElement("div");
+                        t.classList.add("Imgerror-container");
+                        t.innerHTML = `...`;
+                        e.parentNode.insertBefore(t, e.nextSibling);
+                        e.style.display = "none";
+                    } else {
+                        e.classList.remove("ImgLazyLoad-circle");
+                        e.classList.add("ImgLoaded");
+                    }
+                };
 
+                e.src = e.getAttribute("img-src");
+                t.unobserve(e);
+                e.onload = () => i(!1);
+                e.onerror = () => i(!0);
+            }
+        });
+    }, { rootMargin: "0px 0px 500px 0px" });
+
+    document.querySelectorAll("[img-src]").forEach(o => t.observe(o));
 });
