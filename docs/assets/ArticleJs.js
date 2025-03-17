d8b755d7fdb1bfd6a24e7467f674f6242d4a3cf6
planenalp <null>
2025-03-17 17:07:23 +0000

    🎉auto update by Gmeek action

--- docs/assets/ArticleJs.js
+++ docs/assets/ArticleJs.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	//////////////// 引入fancybox所需的css文件以及所需的绑定函数 start ////////////////
 	document.head.appendChild(Object.assign(document.createElement('link'), {
 		rel: 'stylesheet',
-		href: 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css'
+		href: 'https://planenalp.github.io/assets/fancybox.css'
 	}));
 	Fancybox.bind('[data-fancybox="gallery"]', {});
 	//////////////// 引入fancybox所需的css文件以及所需的绑定函数 end ////////////////
