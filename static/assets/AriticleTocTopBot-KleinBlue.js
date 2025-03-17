006c4965c738589e31d38c82047a89e416d7da13
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-18 04:46:16 +0800

    Update AriticleTocTopBot-KleinBlue.js

--- static/assets/AriticleTocTopBot-KleinBlue.js
+++ static/assets/AriticleTocTopBot-KleinBlue.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function() {
     ////////////////// 引入fancybox的CSS及绑定（代码A） start ////////////////
     const fancyboxLink = Object.assign(document.createElement('link'), {
         rel: 'stylesheet',
-        href: 'https://raw.githubusercontent.com/planenalp/ui/main/dist/fancybox/fancybox.css'
+        href: 'https://planenalp.github.io/assets/fancybox.umd.js'
     });
     document.head.appendChild(fancyboxLink);
     // 绑定 Fancybox（确保 Fancybox 脚本已引入）
