001eeb78a68eb291b8d2db8757782c465678b43a
planenalp <null>
2025-03-18 03:57:45 +0000

    🎉auto update by Gmeek action

--- docs/assets/AriticleTocTopBot-KleinBlue.js
+++ docs/assets/AriticleTocTopBot-KleinBlue.js
@@ -318,14 +318,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // 创建 TOC 切换图标
     const tocIcon = document.createElement('div');
     tocIcon.className = 'toc-icon';
-    tocIcon.innerHTML = '<svg viewBox="0 0 24 24"><path d="M3 12h18M3 6h18M3 18h18"/></svg>';
+    //tocIcon.innerHTML = '<svg viewBox="0 0 24 24"><path d="M3 12h18M3 6h18M3 18h18"/></svg>'; //汉堡图标
     tocIcon.addEventListener('click', e => {
         e.stopPropagation();
         const tocElement = document.querySelector('.toc');
         tocElement.classList.toggle('show');
         tocIcon.classList.toggle('active');
         tocIcon.innerHTML = tocElement.classList.contains('show')
-            ? '<svg viewBox="0 0 24 24"><path d="M20 4L4 20M4 4l16 16"/></svg>' //X图标
+            ? '<svg viewBox="0 0 24 24"><path d="M4 4l16 16M4 20L20 4"/></svg>' //X图标
             : '<svg viewBox="0 0 24 24"><path d="M3 12h18M3 6h18M3 18h18"/></svg>'; //汉堡图标
     });
     document.body.appendChild(tocIcon);
