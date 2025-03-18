e1e6655fbd983d4af9ad3546d9f4644e9feb2d5d
planenalp <null>
2025-03-18 23:43:35 +0000

    🎉auto update by Gmeek action

--- docs/assets/AriticleTocTopBot-planenalp.js
+++ docs/assets/AriticleTocTopBot-planenalp.js
@@ -129,11 +129,10 @@ document.addEventListener("DOMContentLoaded", function() {
 			line-height: 1.5;
 			text-decoration: none;
 		}
-  		/* 鼠标悬停高亮 */
 		.toc a:hover {
 			/* border-radius: 6px; */
 			background-color: var(--toc-a-hover);
-			color: var(--toc-icon-hover-bgColor);
+			color: #FFFFFF;
 			transform:translate(1px,1px);
 		}
   		/* 弹出菜单图标 */
@@ -182,7 +181,6 @@ document.addEventListener("DOMContentLoaded", function() {
 			stroke-linecap: round;
 			stroke-linejoin: round;
 		}
-    		/* h1 标题左侧高亮竖条 */
 		.toc-h1{
 			position: relative;
 			padding-left: 10px;
@@ -197,7 +195,9 @@ document.addEventListener("DOMContentLoaded", function() {
 			background-color: var(--toc-h1-after-bgColor);
 			transform: translateY(-50%);
 		}
-
+  		.toc-link.toc-active {
+			background-color: var(--toc-a-hover);
+		}
 		.back-to-top, .back-to-bot {
 		    position: fixed;
 		    right: 20px;
