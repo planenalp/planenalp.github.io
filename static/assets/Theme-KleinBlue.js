fb90822a2e6b526862aa63f287cc79a341d7fc5e
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-18 08:05:21 +0800

    Update Theme-KleinBlue.js

--- static/assets/Theme-KleinBlue.js
+++ static/assets/Theme-KleinBlue.js
@@ -1,3 +1,4 @@
+document.addEventListener('touchstart', function() {}, false);
 document.addEventListener('DOMContentLoaded', function() {    
     let currentUrl = window.location.pathname;
     //let currentHost = window.location.hostname;
