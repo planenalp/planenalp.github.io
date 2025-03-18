5e11c24c991f3fe2fa215c699054533d05a457c2
planenalp <null>
2025-03-18 06:08:26 +0000

    🎉auto update by Gmeek action

--- docs/assets/AriticleTocTopBot-planenalp.js
+++ docs/assets/AriticleTocTopBot-planenalp.js
@@ -194,49 +194,7 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
 		}
 
-		.back-to-top {
-			position: fixed;
-			bottom: 1%;
-			right: 15px;
-			background-color: rgba(255, 255, 255, 0.8);
-			color: #333;
-			border: none;
-			border-radius: 50%;
-			cursor: pointer;
-			box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-			width: 50px;
-			height: 50px;
-			font-size: 30px;
-			z-index: 10000;
-			transform: translateY(20px); /* 初始位置稍微偏下 */
-			opacity: 0;
-			visibility: hidden;
-			transition: transform 0.3s ease, opacity 0.3s ease, visibility 0s linear 0.3s; /* 过渡效果 */
-			align-items: center;
-			justify-content: center;
-		}
-		
-		.back-to-top.show {
-			transform: translateY(0); /* 恢复正常位置 */
-			opacity: 1;
-			visibility: visible;
-			transition: transform 0.3s ease, opacity 0.3s ease;
-		}
-		
-		.back-to-top:hover {
-		 transform: scale(1.1);
-		}
-		.back-to-top svg {
-		 width: 24px;
-		 height: 24px;
-		}
 		@media (max-width: 768px) {
-		 .back-to-top {
-			bottom: 2%;
-			width: 40px;
-			height: 40px;
-			font-size: 20px;
-		 }
 		 .toc-icon {
 			bottom: 7%;
 		 }
@@ -268,36 +226,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 	});
 
-	// 创建ToTop按钮
-	const btn = document.createElement('button');
-	btn.className = 'back-to-top';
-	btn.innerHTML =
-		'<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 19V5M5 12l7-7 7 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
-	document.body.appendChild(btn);
 
-	// 点击事件处理
-	btn.addEventListener('click', () => {
-		window.scrollTo({
-			top: 0,
-			behavior: 'smooth'
-		});
-	});
-
-	// 滚动事件处理
-	function toggleButtonVisibility() {
-		if (window.pageYOffset > 300) {
-			btn.classList.add('show');
-		} else {
-			btn.classList.remove('show');
-		}
-	}
-
-	//监听ToTop按钮执行函数
-	window.addEventListener('scroll', toggleButtonVisibility);
-	window.addEventListener('resize', toggleButtonVisibility);
-
-	// 执行函数
-	toggleButtonVisibility();
 	
 	function highlightTOC() {
 		const tocLinks = document.querySelectorAll('.toc-link');
