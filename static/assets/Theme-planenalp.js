a9bc95f5162a7689bdedc93d324263c656bc6480
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-21 03:01:16 +0800

    Update Theme-planenalp.js

--- static/assets/Theme-planenalp.js
+++ static/assets/Theme-planenalp.js
@@ -29,11 +29,11 @@ document.addEventListener('DOMContentLoaded', function() {
         [data-color-mode=dark][data-dark-theme=dark],
         [data-color-mode=dark][data-dark-theme=dark]::selection {
             --body-bgColor: #21262db3; /* 黑色背景，透明度70% */
-            --header-bgColor: #002FA7;
+            --header-bgColor: unset;
             --blogTitle-color: #FFFFFF;
             --subTitle-color: #FFFFFF;
             --SideNav-bgColor: #21262dcc; /* 黑色背景，透明度80% */
-            --btnSideNav-hover-bgColor: unset; /* 高亮颜色默认 */
+            --btnSideNav-hover-bgColor: #002fa7; /* 高亮颜色默认 */
             --box-shadow: 0 0 transparent; /* 添加阴影 */
             --avatarURL: url("https://planenalp.github.io/avatar-white.svg");
         }
