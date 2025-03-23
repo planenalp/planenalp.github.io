fde0aa0244d829cb9bfa3a6295c7a0ac8b298d30
planenalp <null>
2025-03-23 12:44:11 +0000

    🎉auto update by Gmeek action

--- docs/assets/Theme-planenalp.js
+++ docs/assets/Theme-planenalp.js
@@ -278,7 +278,11 @@ document.addEventListener('DOMContentLoaded', function() {
         /* 按钮悬停色 */
         .btn-invisible:hover {
             background-color: var(--btn-hover-bgColor);
-            color: var(--text-hover-color);
+        }
+         */
+
+         .btn:hover {
+            background-color: var(--btn-hover-bgColor);
         }
 
         /* 按钮图标色 */
