21960ea20c681d888703836c55fd974b86b5bcb3
planenalp <null>
2025-03-24 02:59:29 +0000

    🎉auto update by Gmeek action

--- docs/assets/Theme-planenalp.js
+++ docs/assets/Theme-planenalp.js
@@ -653,7 +653,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 display: unset !important;
             }
             .LabelName{
-                display: none !important;
+                display: unset !important;
             }
         }
         
