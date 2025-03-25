9bd4956cfa093cce726223d39ef5b26fdb03b646
planenalp <null>
2025-03-25 20:40:33 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [3](https://planenalp.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 57281 
-### :alarm_clock: 2025-03-26 01:37:55 
+### :alarm_clock: 2025-03-26 04:40:33 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/Theme-planenalp.js
+++ docs/assets/Theme-planenalp.js
@@ -192,8 +192,8 @@ document.addEventListener('DOMContentLoaded', function() {
     globalStyle.innerHTML = `
     /* 通用默认亮主题配色 */
     :root {
-        /* --bgURL: url("https://planenalp.github.io/bgLight.webp"); */
-        --avatarURL: url("https://planenalp.github.io/avatar-blue.svg");
+        /* --bgURL: url("https://planenalp.github.io/logo/bgLight.webp"); */
+        --avatarURL: url("https://planenalp.github.io/logo/avatar-blue.svg");
         --body-bgColor: #ffffffb3;
         --box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
         --header-bgColor: #002fa7;
@@ -212,8 +212,8 @@ document.addEventListener('DOMContentLoaded', function() {
     [data-color-mode=light][data-light-theme=dark]::selection,
     [data-color-mode=dark][data-dark-theme=dark],
     [data-color-mode=dark][data-dark-theme=dark]::selection {
-        /* --bgURL: url("https://planenalp.github.io/bgDark.webp"); */
-        --avatarURL: url("https://planenalp.github.io/avatar-whitesmoke.svg");
+        /* --bgURL: url("https://planenalp.github.io/logo/bgDark.webp"); */
+        --avatarURL: url("https://planenalp.github.io/logo/avatar-whitesmoke.svg");
         --body-bgColor: #0d1117b3;
         --box-shadow: 0 0 transparent;
         --header-bgColor: #002fa7;
@@ -307,7 +307,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         /* header布局 */
         #header {
-            height: 165px;
+            height: 175px;
             position: relative; /* 父元素 #header 设置定位 */
             border-bottom: unset;
             margin-bottom: unset;
@@ -317,7 +317,6 @@ document.addEventListener('DOMContentLoaded', function() {
             position: absolute;
             left: 50%;
             transform: translateX(-50%);
-            margin-top: 10px;
             display: flex;
             flex-direction: column;
             align-items: center;
@@ -332,6 +331,8 @@ document.addEventListener('DOMContentLoaded', function() {
         .avatar {
             width: 100px;
             height: 100px;
+            margin-top: 10px;
+            margin-bottom: 10px;
             transition: 0.1s !important; /* 强制指定动画时长 */
             object-fit: unset !important; /* 强制清除自动缩放 */
             background-color: transparent !important; /* 强制清除背景颜色 */
@@ -375,7 +376,7 @@ document.addEventListener('DOMContentLoaded', function() {
         /* 按钮位置 */
         .title-right {
             margin: unset; /* 重置原参数 */
-            margin-top: 190px; /* 用百分比会崩 */
+            margin-top: 200px; /* 用百分比会崩 */
             margin-left: 50%;
             transform: translateX(-50%);
             position: absolute;
@@ -463,7 +464,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 height:80px !important; /* 标题缩小 */
             }
             #header {
-                height: 125px; /* 标题缩小 */
+                height: 135px; /* 标题缩小 */
                 padding-bottom: unset; /* 标题缩小 */
             }
 
@@ -472,7 +473,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             .title-right {
-                margin-top: 150px; /* 标题缩小 */
+                margin-top: 160px; /* 标题缩小 */
             }
             .blogTitle{
                 display:unset !important;
