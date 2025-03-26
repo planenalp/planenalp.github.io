160d2a63c7fe0ada80c2b37009cab35bb38dd611
planenalp <null>
2025-03-26 22:24:20 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [4](https://planenalp.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 114503 
-### :alarm_clock: 2025-03-27 06:22:44 
+### :alarm_clock: 2025-03-27 06:24:20 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/Theme-planenalp.js
+++ docs/assets/Theme-planenalp.js
@@ -647,20 +647,21 @@ document.addEventListener('DOMContentLoaded', function() {
         #themeSwitch {
             color: var(--themeSwitch-color);
         }
-
-        /* 评论按钮悬停字色 */
+        
+         /* 评论按钮悬停 */
         @media (any-hover: hover) {
             #cmButton:hover {
                 background-color: var(--btn-hover-bgColor);
                 color: var(--text-hover-color);
             }
         }
-
+        
+        /* 评论按钮按压 */
         #cmButton:active {
             background-color: var(--btn-hover-bgColor);
             color: var(--text-hover-color);
         }
-
+        
         /* 重新定义 max-width: 768px 参数下的值，原为 600px */
         @media (max-width: 768px) {
             body {
