1755d60b5e44680d6e775e1e05e1b814622c6f3c
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-27 16:08:44 +0800

    Update fancyboxLoad-planenalp.js

--- static/assets/fancyboxLoad-planenalp.js
+++ static/assets/fancyboxLoad-planenalp.js
@@ -99,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     ////////////////// Fancybox 绑定及配置 end ////////////////
 
-
+/*
 // ==================== 手动插入外链图片 START ====================
 if (document.querySelector(".markdown-body")) {
     const post_body = document.querySelector(".markdown-body").innerHTML;
@@ -112,6 +112,20 @@ if (document.querySelector(".markdown-body")) {
     }
 }
 // ==================== 手动插入外链图片 END ====================
+*/
+
+
+
+
+
+// 假设 post_body 为包含文章内容的 HTML 字符串
+if (post_body.indexOf('<code class="notranslate">Gmeek-imgbox') !== -1) {
+    post_body = post_body.replace(/<p>\s*<code class="notranslate">Gmeek-imgbox="([^"]+)"<\/code>\s*<\/p>/g, function(match, p1) {
+        return '<div class="ImgLazyLoad-circle"></div>\n<img data-fancybox="gallery" img-src="' + p1 + '">';
+    });
+}
+
+
 
 
 
