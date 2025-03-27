32345077849bebdbba07e3aa24c0ddd3a91dd2d3
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-27 16:12:37 +0800

    Update fancyboxLoad-planenalp.js

--- static/assets/fancyboxLoad-planenalp.js
+++ static/assets/fancyboxLoad-planenalp.js
@@ -117,14 +117,14 @@ if (document.querySelector(".markdown-body")) {
 
 
 
-
+/*
 // 假设 post_body 为包含文章内容的 HTML 字符串
 if (post_body.indexOf('<code class="notranslate">Gmeek-imgbox') !== -1) {
     post_body = post_body.replace(/<p>\s*<code class="notranslate">Gmeek-imgbox="([^"]+)"<\/code>\s*<\/p>/g, function(match, p1) {
         return '<div class="ImgLazyLoad-circle"></div>\n<img data-fancybox="gallery" img-src="' + p1 + '">';
     });
 }
-
+*/
 
 
 
