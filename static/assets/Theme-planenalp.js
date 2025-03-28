4eb0f96db94e6a5e7e5662321018e00fa0c715bd
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-28 22:55:23 +0800

    Update Theme-planenalp.js

--- static/assets/Theme-planenalp.js
+++ static/assets/Theme-planenalp.js
@@ -560,11 +560,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
         /* 标签（背景色不能关闭，因为 .LabelName 字色没法自定义，除非隐藏 .LabelName） */
         /* 方案1：首页隐藏 .LabelName + 仅保留 .LabelTime（去掉背景色，字色改黑色/灰色+ hover + active 白色），搜索页按方案2 */
-        /* 方案2：首页+搜索页边框透明，hover + active 边框 #f5f5f5 */
+        /* 方案2：首页+搜索页边框透明，hover + active 边框 #f5f5f5 （考虑全部颜色统一）*/
         .Label {
             border-radius: unset; /* 圆角关闭 */
             border: 1px solid transparent; /* 默认隐藏边框 */
             /* background-color: transparent !important; 关闭标签背景色 */
+            color: #002fa7cc;
         }
         
         /* 重新定义 max-width: 768px 参数下的值，原为 600px */
