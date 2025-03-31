e444553aede46c4518300996860238b0447f225b
planenalp <111256256+planenalp@users.noreply.github.com>
2025-04-01 05:20:26 +0800

    Update Theme-planenalp.js

--- static/assets/Theme-planenalp.js
+++ static/assets/Theme-planenalp.js
@@ -549,6 +549,13 @@ document.addEventListener('DOMContentLoaded', function() {
             /* border: 1px solid transparent; 默认隐藏边框 */
             border: unset;
             box-shadow: var(--box-shadow); /* 阴影 */
+            -webkit-tap-highlight-color: transparent; /* 修复某些安卓设备的点击外框 */
+            -webkit-touch-callout: none; /* 禁用 iOS 长按弹出菜单 */
+            -webkit-user-select: none; /* 禁用 iOS Safari 和其他 WebKit 内核浏览器的文本选择 */
+            -moz-user-select: none; /* 适用于 Firefox */
+            -ms-user-select: none; /* 适用于 IE10+ 和 Edge */
+            user-select: none; /* 标准语法 */
+            outline: none !important; /* 解决按压边框闪烁 */
         }
         
         /* 重新定义 max-width: 768px 参数下的值，原为 600px */
@@ -802,6 +809,13 @@ document.addEventListener('DOMContentLoaded', function() {
         .subnav-search button {
             border: unset; /* 去除边框 */
             border-radius: unset; /* 去除圆角 */
+            webkit-tap-highlight-color: transparent; /* 修复某些安卓设备的点击外框 */
+            -webkit-touch-callout: none; /* 禁用 iOS 长按弹出菜单 */
+            -webkit-user-select: none; /* 禁用 iOS Safari 和其他 WebKit 内核浏览器的文本选择 */
+            -moz-user-select: none; /* 适用于 Firefox */
+            -ms-user-select: none; /* 适用于 IE10+ 和 Edge */
+            user-select: none; /* 标准语法 */
+            outline: none !important; /* 解决按压边框闪烁 */
         }
 
         /* 搜索失败字符 */
@@ -979,6 +993,7 @@ document.addEventListener('DOMContentLoaded', function() {
             border-radius: unset; /* 圆角关闭 */
             /* border: 1px solid transparent; 默认隐藏边框 */
             border: unset; /* 取消边框 */
+            box-shadow: var(--box-shadow); /* 阴影 */
             webkit-tap-highlight-color: transparent; /* 修复某些安卓设备的点击外框 */
             -webkit-touch-callout: none; /* 禁用 iOS 长按弹出菜单 */
             -webkit-user-select: none; /* 禁用 iOS Safari 和其他 WebKit 内核浏览器的文本选择 */
@@ -986,6 +1001,11 @@ document.addEventListener('DOMContentLoaded', function() {
             -ms-user-select: none; /* 适用于 IE10+ 和 Edge */
             user-select: none; /* 标准语法 */
             outline: none !important; /* 解决按压边框闪烁 */
+
+        }
+
+        /* 顶部 #taglabel  */
+        #taglabel .Label {
             box-shadow: var(--box-shadow); /* 阴影 */
         }
 
@@ -1004,7 +1024,6 @@ document.addEventListener('DOMContentLoaded', function() {
             color: var(--text-hover-color) !important;
             transform: scale(0.9);
             transition: 0.1s ease;
-            box-shadow: var(--box-shadow); /* 阴影 */
         }
 
         /* 重新定义 max-width: 768px 参数下的值，原为 600px */
