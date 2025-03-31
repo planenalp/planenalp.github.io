162f9d4ec6ef7991582edf113a5e82b23da3036c
planenalp <111256256+planenalp@users.noreply.github.com>
2025-04-01 04:52:33 +0800

    Update Theme-planenalp.js

--- static/assets/Theme-planenalp.js
+++ static/assets/Theme-planenalp.js
@@ -681,6 +681,13 @@ document.addEventListener('DOMContentLoaded', function() {
             border-radius: unset; /* 去除圆角 */
             border: unset; /* 去除边框 */
             box-shadow: var(--box-shadow); /* 阴影 */
+            -webkit-tap-highlight-color: transparent; /* 修复某些安卓设备的点击外框 */
+            -webkit-touch-callout: none; /* 禁用 iOS 长按弹出菜单 */
+            -webkit-user-select: none; /* 禁用 iOS Safari 和其他 WebKit 内核浏览器的文本选择 */
+            -moz-user-select: none; /* 适用于 Firefox */
+            -ms-user-select: none; /* 适用于 IE10+ 和 Edge */
+            user-select: none; /* 标准语法 */
+            outline: none !important; /* 解决按压边框闪烁 */
         }
         
          /* 评论按钮悬停 */
