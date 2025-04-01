69aa39b073ef4af6d753ba76fad9938a86efb891
planenalp <null>
2025-04-01 18:59:31 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [18](https://planenalp.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 94268 
-### :alarm_clock: 2025-04-02 02:53:21 
+### :alarm_clock: 2025-04-02 02:59:31 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/AriticleTocTopBot.js
+++ docs/assets/AriticleTocTopBot.js
@@ -16,7 +16,6 @@ function loadResource(type, attributes) {
 function createTOC() {
     const tocElement = document.createElement('div');
     tocElement.className = 'toc';
-    // 立即设置内联样式，确保初始隐藏
     tocElement.style.opacity = '0';
     tocElement.style.visibility = 'hidden';
     document.body.appendChild(tocElement);
